27b3ddbe8c17e5d94bbd161b8c36824e75b968bb
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-10 20:15:25 +0600

    readme file edit and netlify rounting problem solve

--- README.md
+++ README.md
@@ -3,19 +3,18 @@
 Beacome a punctual doctor! it's help you to managament you daily professional works.
 
 ## Features
-- Store Food, Fruit and vegitable  
-- add itmes
-- Authentication
-- Authorization 
-- Manage Inventories(details and detele)
+- home page
+- Our services
+- Inforamtion
+- submit comment
 
 ## Tools
 - REACT
 - firebase
 - firebase Hooks 
-- REACT BOOTSTRAPS
 - React Router 
-- 
+- REACT Tailwind
+- REACT dasiyUI 
 
 ## website live link
-[Live view]()
+[Live view](doctor-bhai-yebenling.netlify.app)
--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import footerImage from '../../assets/images/footer.png';
 
 const Footer = () => {
     return (
-        <footer>
-            <div className="footer p-10 bg-base-200 text-base-content">
+        <footer style={{background: `url(${footerImage},)`, backgroundSize:'cover'}}>
+            <div className="footer p-10 text-base-content">
                 <div>
                     <span className="footer-title">Services</span>
                     <Link to='' className="link link-hover">Branding</Link>
@@ -39,7 +40,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className='text-center p-4 bg-base-300 text-base-content'>
+            <div className='text-center p-4 text-base-content'>
                 <p>Copyright © 2022 - All right reserved by zaman</p>
             </div>
         </footer>
