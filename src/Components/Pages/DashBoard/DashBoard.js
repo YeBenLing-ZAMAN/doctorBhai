8c4b0e331a6c27e8884ef37db7358e41b267fb5f
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-15 16:51:28 +0600

    my appointment section implemention with get req and load data from  appointment db

--- src/Components/Pages/DashBoard/DashBoard.js
+++ src/Components/Pages/DashBoard/DashBoard.js
@@ -4,24 +4,29 @@ import { Link, Outlet } from 'react-router-dom';
 const DashBoard = () => {
     return (
         <div class="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-    {/* <!-- Page content here --> */}
-    <h2 className='text-purple-500 text-2xl'>Dash Board</h2>
-    <Outlet></Outlet>
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-  
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-      {/* <!-- Sidebar content here --> */}
-      <li><Link to='/dashboard'>Appointment</Link></li>
-      <li><Link to='/dashboard/review'>My review</Link></li>
-    </ul>
-  
-  </div>
-</div>
+            <input id="dashBoard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content bg-gradient-to-r from-primary to-secondary">
+                {/* <!-- Page content here --> */}
+                <div className='flex items-center justify-around	'>
+                    <h2 className='text-purple-500 text-2xl  p-3 text-left lg:text-right'>welcome your Dash Board</h2>
+
+                    {/* dashboad sidebar button */}
+                    <label for="dashBoard-sidebar" class="btn btn-primary drawer-button lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                </div>
+                <Outlet></Outlet>
+            </div>
+            <div class="drawer-side">
+                <label for="dashBoard-sidebar" class="drawer-overlay"></label>
+                <ul class="menu p-4 overflow-y-auto w-48 bg-base-200 text-base-content ">
+                    {/* <!-- Sidebar content here --> */}
+                    <li><Link to='/dashboard'>Appointment</Link></li>
+                    <li><Link to='/dashboard/review'>My review</Link></li>
+                </ul>
+
+            </div>
+        </div>
     );
 };
 
--- src/Components/Pages/DashBoard/MyAppointment.js
+++ src/Components/Pages/DashBoard/MyAppointment.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyAppointment = () => {
+    const [user] = useAuthState(auth);
+    const [appointment, setAppointment] = useState([]);
+    useEffect(() => {
+        if (true) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppointment(data))
+        }
+    }, [user])
     return (
         <div>
-            <h2>this is my appointment</h2>
+            <h2 className='text-xl text-right px-5'>Your padding appointment remaining: {appointment.length}</h2>
+            <div class="overflow-x-auto p-3">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Treatment</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {
+                            appointment.map((a,index)=> <tr>
+                                <th>{index + 1}</th>
+                                <td>{a.patientName}</td>
+                                <td>{a.treatment}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                            </tr> )
+                        }
+                       
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
