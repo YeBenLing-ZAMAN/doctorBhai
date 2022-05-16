46537b763c7e6ceb2f61f538c775f9ed629195ac
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-16 10:10:33 +0600

    login jwt token set and navigate

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -4,6 +4,7 @@ import auth from '../../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../../hooks/useToken';
 
 
 const Login = () => {
@@ -15,16 +16,20 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+
+    /* token */
+    const[token] = useToken(user || gUser);
+
     const navigate = useNavigate();
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
-        if (user || gUser) {
-            console.log('user', user);
+        if (token) {
+            // console.log('user', user);
             navigate(from, { replace: true });
         }
-    }, [user,gUser, from, navigate])
+    }, [token, from, navigate])
 
 
     const onSubmit = async data => {
